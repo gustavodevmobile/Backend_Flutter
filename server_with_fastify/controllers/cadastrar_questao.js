2ef751b2ea37bcd9b428bb77a3c2477419db9070
Gustavo Correa <205154733+gustavodevmobile@users.noreply.github.com>
2025-04-06 23:16:18 -0300

    edição ok

--- server_with_fastify/controllers/cadastrar_questao.js
+++ server_with_fastify/controllers/cadastrar_questao.js
@@ -1,21 +1,27 @@
 import fs from "fs";
+
 import "dotenv/config";
 import Database from "../database/database.js";
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const screen_cadastrar_questao = async (req, reply) => {
   return await reply.render("templates/cadastrar");
 };
 
 export const cadastrar_questao = async function (req, reply) {
+  const imagePath = path.resolve(__dirname, "../images/" + req.file.filename);
   let image;
   let filename;
-  console.log("req.file.filename", req.file.filename);
+  //console.log("req.file.filename", req.file.filename);
   if (!req.file || !req.file.filename) {
     req.body.image = "sem imagem";
     image = req.body.image;
     filename = null;
   } else {
-    image = fs.readFileSync("./images/" + req.file.filename);
+    image = fs.readFileSync(imagePath);
     filename = req.file.filename;
   }
 
--- server_with_fastify/controllers/editar.js
+++ server_with_fastify/controllers/editar.js
@@ -1,5 +1,6 @@
 //import multer from "fastify-multer";
-import fs from "fs";
+import fsSync, { access } from "fs";
+import fs from "fs/promises";
 import "dotenv/config";
 import Database from "../database/database.js";
 import { fileURLToPath } from "url";
@@ -7,38 +8,57 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const renamePath = async (oldPath, newPath) => {
+  try {
+    await fs.rename(oldPath, newPath);
+  } catch (error) {
+    console.log("Erro ao renomar arquivo:", error.message);
+    throw error; 
+  }
+};
+
+const updateImage = async (newPath) => {
+  try {
+    const newImage = await fs.readFile(newPath);
+    return newImage;
+  } catch (error) {
+    console.log("Erro ao atualizar arquivo:", error.message);
+    throw error; 
+  }
+};
+
 export const editar_questao = async (req, reply) => {
+  let nameImageDirUpdated;
+  let newImage;
   try {
     const result = await Database.findByPk(req.body.id);
     const imageBuffer = result["dataValues"]["image"];
     const nameImageDir = result["dataValues"]["nameImageDir"];
-    let nameImageDirUpdated;
-    let image;
+    const oldPath = path.resolve(__dirname, "../images/" + nameImageDir);
+    const newPath = path.resolve(__dirname, "../images/" + req.file.filename);
 
-    if (req.file.filename != undefined) {
+    console.log("nameImageDir", nameImageDir);
+    if (req.file.filename && req.file) {
       try {
-        const oldPath = path.resolve(__dirname, "../images/" + nameImageDir);
-        const newPath = path.resolve(
-          __dirname,
-          "../images/" + req.file.filename
-        );
-        console.log("req.file.filename", req.file.filename);
-        if (fs.existsSync(oldPath)) {
-          fs.renameSync(oldPath, newPath, (err) => {
-            if (err) {
-              console.log("Erro ao renomear o arquivo:", err);
-            }
-          });
-        }
-        image = fs.readFileSync("./images/" + req.file.filename);
-        console.log("Arquivo renomeado com sucesso:", newPath);
+        await fs.access(newPath);
+        newImage = await updateImage(newPath);
+        nameImageDirUpdated = req.file.filename;
+        console.log("Arquivo atualizado com sucesso!", newPath);
       } catch (error) {
         console.error("Erro ao acessar o diretório:", error);
       }
+      try{
+        await fs.access(oldPath);
+        await renamePath(oldPath, newPath);
+        console.log("Arquivo renomeado com sucesso!", oldPath);
+      }catch (error) {
+        console.log("Erro ao renomear arquivo:", error.message);
+      }
       
     } else {
-      image = imageBuffer;
+      newImage = imageBuffer;
       nameImageDirUpdated = nameImageDir;
+      
     }
     await Database.update(
       {
@@ -47,7 +67,7 @@ export const editar_questao = async (req, reply) => {
         displice: req.body.displice,
         subject: req.body.subject,
         question: req.body.question,
-        image: image,
+        image: newImage, 
         nameImageDir: nameImageDirUpdated,
         answer: req.body.answer,
         alternativeA: req.body.altA,
@@ -66,15 +86,14 @@ export const editar_questao = async (req, reply) => {
 
 export const screen_editar_questao = (req, reply) => {
   try {
-    let image;
-    let nameImage;
+    // let image;
+    // let nameImage;
     Database.findByPk(req.params.id).then((result) => {
       //console.group(result.nameImageDir);
       const nameImageDir = result["dataValues"]["nameImageDir"];
       //console.log('nameImageDir', nameImageDir);
       const imageBase64 = result["dataValues"]["image"].toString("base64");
       result["dataValues"]["image"] = imageBase64;
-      
 
       reply.render("templates/editar", {
         id: result.id,
